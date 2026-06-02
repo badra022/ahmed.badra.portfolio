3e37e0271c626167debf0f51a487bd30e8433266
ahmed.badra <42746335+badra022@users.noreply.github.com>
2026-06-02 05:23:48 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,10 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   base: '/ahmed.badra.portfolio/',
 })
